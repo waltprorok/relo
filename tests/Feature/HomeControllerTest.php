67855750ea2fd8a33acff097948faf214f8b9594
Walter Prorok <waltprorok@gmail.com>
2025-04-08 13:45:16 -0400

    RL-5 added unit test

---
+++ tests/Feature/HomeControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Mail;
+use Tests\TestCase;
+
+class HomeControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Mail::fake();
+    }
+
+    public function test_home_page_url_200()
+    {
+        $response = $this->get('/');
+
+        $response->assertOk();
+    }
+
+    public function test_home_page_route_200()
+    {
+        $response = $this->get(route('home'));
+
+        $response->assertOk();
+    }
+}
