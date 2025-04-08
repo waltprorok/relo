a6580e69efd33627107f34d84f5b0b8b368d5d98
Walter Prorok <waltprorok@gmail.com>
2025-04-08 13:31:10 -0400

    RL-3 cleaned up home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,4 +26,9 @@ class HomeController extends Controller
     {
         return view('webapp.home');
     }
+
+    public function contact()
+    {
+        //
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\Honeypot\ProtectAgainstSpam;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('marketing.home');
-});
-
 Auth::routes();
 
+Route::view('/', 'marketing.home')->name('home');
+Route::post('/contact', [App\Http\Controllers\HomeController::class, 'contact'])->name('contact')->middleware(ProtectAgainstSpam::class);
+
 Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+
+
