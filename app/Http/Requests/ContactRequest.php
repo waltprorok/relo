c8793f7e1e830d544e6d27d4519bd40d392014b0
Walter Prorok <waltprorok@gmail.com>
2025-04-28 13:59:00 -0400

    RL-50 request auth true

--- app/Http/Requests/ContactRequest.php
+++ app/Http/Requests/ContactRequest.php
@@ -9,10 +9,10 @@ class ContactRequest extends FormRequest
     /**
      * Determine if the user is authorized to make this request.
      */
-//    public function authorize(): bool
-//    {
-//        return true;
-//    }
+    public function authorize(): bool
+    {
+        return true;
+    }
 
 
     public function rules(): array
