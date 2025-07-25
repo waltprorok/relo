de0e4e9c81cc293d28a2338695f1bf5582c82603
Walter Prorok <waltprorok@gmail.com>
2025-07-25 16:36:16 -0400

    RL-68 added privacy and terms pages

---
+++ resources/views/marketing/privacy-policy.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.app')
+@section('content')
+
+    <section class="section" id="terms-and-conditions">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2>Privacy Policy</h2>
+                    <p><strong>Effective:</strong> July 25, 2025</p>
+
+                    <h2>Introduction</h2>
+                    <p>
+                        The following terms and conditions govern your use of this website (<a href="{{ config('app.url') }}">https://reloagentconnect.com</a>), a product of Relo Agent Connect and
+                        Real Estate Services ("Relo Agent Connect"). Your use of this website and its content constitutes your acceptance of the terms and conditions stated below.
+                    </p>
+                    <p>
+                        This website provides information and content owned or licensed by Relo Agent Connect. Terms such as "we" and "our" refer to Relo Agent Connect, and “you” and “your” refer to any
+                        individual, company, or entity using this website.
+                    </p>
+
+                    <h2>Additional Terms</h2>
+                    <p>
+                        These terms apply only to your use of this website and not to other services or products offered by Relo Agent Connect. Additional terms may apply to specific services or products.
+                        In the case of conflicting terms, the more specific terms shall govern. If you have an account with Relo Agent Connect, your relationship is governed by applicable account
+                        agreements, which take precedence over these terms in case of a conflict.
+                    </p>
+
+                    <h2>Ownership and Use of Content</h2>
+                    <p>
+                        All content on this website (text, logos, newsletters, graphics, software, forms, reports, media files, trademarks, etc.) is owned or licensed by Relo Agent Connect and protected by
+                        intellectual property laws. You may print or use the content for personal, non-commercial use only, retaining all proprietary notices.
+                    </p>
+                    <p>
+                        Written consent is required to use content elsewhere, including republishing, redistributing, or embedding content within other websites. You may not frame this site’s content
+                        or misuse trademarks or service marks in any way.
+                    </p>
+
+                    <h2>Links</h2>
+                    <p>
+                        Relo Agent Connect does not endorse or take responsibility for third-party websites linked from this site. Following such links is at your own risk. We reserve the right to remove
+                        links upon request from linked site owners.
+                    </p>
+
+                    <h2>Eligibility</h2>
+                    <p>
+                        Not all services or products described on this website may be available in all areas. Eligibility is subject to our discretion and may change without notice.
+                    </p>
+
+                    <h2>No Warranty</h2>
+                    <p>
+                        Use of this website and its content is at your own risk. While we strive for accuracy, the content is provided "as is" and may become outdated. We disclaim all warranties,
+                        express or implied, to the fullest extent permitted by law.
+                    </p>
+
+                    <h2>Security Responsibility</h2>
+                    <p>
+                        Relo Agent Connect is not liable for damages resulting from your use or download of content. It is your responsibility to take precautions, including antivirus protection, to secure
+                        your systems.
+                    </p>
+
+                    <h2>No Liability</h2>
+                    <p>
+                        Relo Agent Connect, its agents, affiliates, or contributors are not liable for any direct, indirect, incidental, or consequential damages arising from your use of this site or its
+                        content.
+                    </p>
+
+                    <h2>Additional Information</h2>
+                    <p>
+                        Except for personal information covered by our Internet Privacy Policy, all information submitted through this website becomes Relo Agent Connect's property. We may use feedback,
+                        ideas, or techniques submitted for any purpose unless otherwise agreed or required by law.
+                    </p>
+
+                    <h2>Changes</h2>
+                    <p>
+                        Relo Agent Connect reserves the right to modify these terms and the content at any time without notice. Continued use of this site implies acceptance of any updated terms.
+                    </p>
+
+                    <h2>Governing Law</h2>
+                    <p>
+                        These terms are governed by the laws of the United States and the State of Utah, without regard to conflict of law principles.
+                    </p>
+
+                    <h2>United States Only</h2>
+                    <p>
+                        The services and information provided on this website are intended for U.S. residents only. Use from other countries is at the user’s own risk and responsibility. Materials may
+                        be subject to U.S. export laws, and downloading content must comply with applicable export regulations.
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection
---
+++ resources/views/marketing/terms-and-conditions.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+@section('content')
+
+    <section class="section" id="terms-and-conditions">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2>Terms and Conditions</h2>
+                    <p><strong>Effective:</strong> July 25, 2025</p>
+
+                    <p>Relo Agent Connect and the affiliated companies of Relo Agent Connect Real Estate and Relocation
+                        Services ("we"), understand how important it is to protect the privacy of certain information collected about our potential customers and customers.
+                    </p>
+
+                    <p>Relo Agent Connect collects information from a variety of sources, including but not limited to forms requesting additional information about
+                        a property or services, online Needs Assessment, newsletters, provided or solicited by Relo Agent Connect. We also analyze statistical patterns that may show preferences for
+                        certain properties, market areas, and services. This information is used to offer better services to our users and to develop tools and
+                        marketing features that help home sellers target buyers whose needs match specific property features.
+                    </p>
+
+                    <h2>Personal Information</h2>
+
+                    <p>Personally identifiable information collected may include:
+                    </p>
+                    <ul>
+                        <li>Name, address, telephone number, email address, social security number, assets, employment, and income.</li>
+                        <li>Data based on your documented feedback or experiences with us or our affiliates.</li>
+                    </ul>
+                    <p>Access to this information is limited to those who need it to provide products or services.
+                    </p>
+
+                    <h2>Sharing Information</h2>
+                    <p>Relo Agent Connect may share certain customer and former customer information among its affiliated companies.</p>
+
+                    <h2>Cookies</h2>
+                    <p>We may use "cookies" to track visitor behavior. A cookie is a small piece of data stored by your browser,
+                        used to personalize navigation, remember preferences, or show contact information for your sales agent as you browse our site.</p>
+
+                    <h2>Links to Other Websites</h2>
+                    <p>Clicking links to third-party websites subjects you to their respective privacy policies.
+                        While we support privacy protection, we are not responsible for third-party practices.</p>
+
+                    <h2>Legal Disclosure</h2>
+                    <p>We may disclose information if we believe it is necessary to identify, contact, or take legal action against someone violating our terms or causing harm to Relo Agent Connect,
+                        its property, or its users. We may also access account information when necessary to enhance our services.
+                    </p>
+
+                    <h2>Privacy Policy Changes</h2>
+                    <p>Relo Agent Connect reserves the right to change this Privacy Policy at any time without notice. Revisions take effect upon posting on our websites.
+                    </p>
+
+                    <h2>Acceptance of Terms</h2>
+                    <p>By using this site, you agree to our Privacy Policy. If you do not agree, please discontinue use. Continued use of this site or affiliated sites constitutes acceptance of this
+                        policy.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection
